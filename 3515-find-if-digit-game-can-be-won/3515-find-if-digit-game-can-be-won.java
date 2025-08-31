e3296da9d26377ce06402148e76e61e6bd2449c9
Dharani dharan M <dharanims123@gmail.com>
2025-08-31 18:07:47 +0530

    Time: 1 ms (57.25%), Space: 44 MB (83.17%) - LeetHub

---
+++ 3515-find-if-digit-game-can-be-won/3515-find-if-digit-game-can-be-won.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canAliceWin(int[] nums) {
+        int ssum = 0;
+        int dsum = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<10){
+                ssum+=nums[i];
+            }
+            else{
+                dsum+=nums[i];
+            }
+        }
+        if(ssum==dsum){
+            return false;
+        }
+        return true;
+    }
+}
